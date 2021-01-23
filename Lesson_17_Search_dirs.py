01441fb8be026510f18d166537725f16a24e919d
MORVf <72212068+MORVf@users.noreply.github.com>
2021-01-23 15:14:55 +0300

    Update Lesson_17_Search_dirs.py

--- Lesson_17_Search_dirs.py
+++ Lesson_17_Search_dirs.py
@@ -6,22 +6,23 @@
 
 """
 
+__all__ = ['FILE_OUT', 'DIR_START', 'lst_dirs']
 __version__ = '1.0.0'
 __author__ = 'Evgenii Mayorov'
 
 import os
 
-file_out = os.path.join("C:\\", "Documents", "file_out.txt")
-dir_start = os.path.join("C:\\", "Documents", "main")  # стартовая директория, в рамках которой будет производиться поиск
+FILE_OUT = os.path.join("C:\\", "Documents", "file_out.txt")
+DIR_START = os.path.join("C:\\", "Documents", "main")  # стартовая директория, в рамках которой будет производиться поиск
 lst_dirs = []
 
-for root_dir, dirs, files in os.walk(dir_start):  # пробегаем все директории и поддиректории в старт. директории
+for root_dir, dirs, files in os.walk(DIR_START):  # пробегаем все директории и поддиректории в старт. директории
     for file in files:
         if '.py' in file:
             lst_dirs += [root_dir[13::]]
             break
 
-with open(file_out, 'w') as f_out:
+with open(FILE_OUT, 'w') as f_out:
     lst_dirs.sort()
     strings_concatenation = "\n".join(lst_dirs)  # конкатенируем все элементы-строки списка, соединяя их символом переноса строки
     f_out.write(strings_concatenation)
