e1b5eb14a6e4af050bf890a352e4575c8e17e909
MORVf <72212068+MORVf@users.noreply.github.com>
2021-01-23 12:24:43 +0300

    Update Lesson_17_Search_dirs.py

--- Lesson_17_Search_dirs.py
+++ Lesson_17_Search_dirs.py
@@ -1,22 +1,27 @@
-'''
+"""Текст задания.
+
 Вам дана файловая структура, состоящая из директорий и файлов.
 Вам необходимо найти в данной в файловой структуре все директории, в которых есть хотя бы один файл с расширением ".py". 
 Ответом на данную задачу будет являться файл со списком таких директорий, отсортированных в лексикографическом порядке.
-'''
+
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Evgenii Mayorov'
 
 import os
 
-file_out = os.path.join("C:\\", "Documents", "file_out.txt")  # выходной файл, куда будут записываться директории
+file_out = os.path.join("C:\\", "Documents", "file_out.txt")
 dir_start = os.path.join("C:\\", "Documents", "main")  # стартовая директория, в рамках которой будет производиться поиск
-lst_dirs = []  # список найденных директорий
+lst_dirs = []
 
 for root_dir, dirs, files in os.walk(dir_start):  # пробегаем все директории и поддиректории в старт. директории
-    for file in files:  # проверяем все файлы в директории
-        if '.py' in file:  # если нашли хотя бы один файл с раширение .py, то
-            lst_dirs += [root_dir[13::]]  # добавляем директорию в список
-            break  # выходим из цикла
+    for file in files:
+        if '.py' in file:
+            lst_dirs += [root_dir[13::]]
+            break
 
 with open(file_out, 'w') as f_out:
-    lst_dirs.sort()  # сортируем полученный список лексикографически
+    lst_dirs.sort()
     strings_concatenation = "\n".join(lst_dirs)  # конкатенируем все элементы-строки списка, соединяя их символом переноса строки
-    f_out.write(strings_concatenation)  # записываем в выходной файл полученную строку
+    f_out.write(strings_concatenation)
