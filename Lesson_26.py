9af78730f71c749ea4395bfc930abefa9db6b882
MORVf <72212068+MORVf@users.noreply.github.com>
2021-01-26 19:48:37 +0300

    Update Lesson_26.py

--- Lesson_26.py
+++ Lesson_26.py
@@ -26,6 +26,6 @@ for line in sys.stdin:
     line = line.rstrip()
     if not line:
         break
-    elif re.findall(SUB_STRING, line) is not None:
+    else:
         line = re.sub(SUB_STRING, 'argh', line, count=1)
         print(line)
